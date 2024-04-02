fe4d2737267028d7a5cc49dd563ea9c0b7837e3d
Naved Uddin <naveduddin83@gmail.com>
2024-04-02 09:38:37 +0530

    creating router

--- scripts/router.js
+++ scripts/router.js
@@ -1,4 +1,40 @@
 export const router = () => {
-    const route = document.querySelectorAll([route]);
-    console.log(route);
+    let doc = document.querySelector('html');
+    
+    // Making routes
+    const routes = {
+        '/' : 'index.html',
+        '/home' : 'home.html',
+    }
+    
+    // Fetching the page
+    const getPage = async (url) => {
+        let response = await fetch(url);
+        let page = await response.text();
+        doc.innerHTML = `${page} <script src="./app.js"><script>`;
+    }
+
+    // looping through each route
+    for (const route in routes) {
+        const path = routes[route];
+        console.log(`Route: ${route}, Path: ${path}`);
+    }
+    
+
+    let route = window.location.pathname;
+    
+    if(route == '/index.html' || route == '/app.html' || route == '/home.html' || route == '/') {
+        history.pushState(null, null, '/');
+    }
+    
+    if(routes[route]) {
+        history.pushState(null, null, route);
+    }
+
+    window.addEventListener('popstate', () => {
+        console.log('State is poped');
+    });
+
+    getPage(routes[route]);
+    
 }
